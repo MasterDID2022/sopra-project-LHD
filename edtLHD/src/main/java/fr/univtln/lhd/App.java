f4442c2127ecd294ed41aa77689f82cb5066102c
Desmondmies <desmondmies@gmail.com>
2022-12-02 12:04:01 +0100

    add sample project

---
+++ edtLHD/src/main/java/fr/univtln/lhd/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.lhd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.err.println( "Hello World!" );
+    }
+}
