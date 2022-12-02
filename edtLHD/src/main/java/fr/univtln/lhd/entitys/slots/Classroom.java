a871a3d6069cce4a353363007753852c1fa5a14e
Desmondmies <desmondmies@gmail.com>
2022-12-02 15:19:36 +0100

    factory Classroom.java

--- edtLHD/src/main/java/fr/univtln/lhd/entitys/slots/Classroom.java
+++ edtLHD/src/main/java/fr/univtln/lhd/entitys/slots/Classroom.java
@@ -1,16 +1,17 @@
 package fr.univtln.lhd.entitys.slots;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class Classroom {
 
     private final String name;
     private final String buildingName; //? might not be a string in database ?
 
-    public Classroom(String name, String buildingName) {
-        this.name = name;
-        this.buildingName = buildingName;
+    public static Classroom getInstance(String name, String buildingName) {
+        return new Classroom(name, buildingName);
     }
-
-    public String getName() { return name; }
-
-    public String getBuildingName() { return buildingName; }
 }
