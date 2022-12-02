78cf9e3e751ef742914e9acb2150b190c190848d
Tom D <tom-domenge@etud.univ-tln.fr>
2022-12-02 17:47:51 +0100

    fix unit tests detection

--- edtLHD/src/test/java/fr/univtln/lhd/entitys/slots/SlotTest.java
+++ edtLHD/src/test/java/fr/univtln/lhd/entitys/slots/SlotTest.java
@@ -2,57 +2,29 @@ package fr.univtln.lhd.entitys.slots;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-class SlotTest {
+public class SlotTest {
 
-    private Classroom getInstanceOfClassroom() { return Classroom.getInstance("Name", "BuildingName"); }
+    private Classroom getInstanceOfClassroom () {
+        return Classroom.getInstance("Name", "BuildingName");
+    }
 
-    private Subject getInstanceOfSubject() { return Subject.getInstance("Name", 70); }
+    private Subject getInstanceOfSubject () {
+        return Subject.getInstance("Name", 70);
+    }
 
-    private Group getInstanceOfGroup() { return Group.getInstance("Name"); }
+    private Group getInstanceOfGroup () {
+        return Group.getInstance("Name");
+    }
 
-    private Slot getInstanceOfSlot(){
+    private Slot getInstanceOfSlot () {
         return Slot.getInstance(Slot.SlotType.CM, getInstanceOfClassroom(), getInstanceOfSubject(), getInstanceOfGroup(), 10);
     }
 
     @Test
-    void testInstanceNotNull(){
+     void testInstanceNotNull () {
         Slot slot = getInstanceOfSlot();
         assertNotNull(slot);
-    }
-
-    @Test
-    void testSlotTypeEquality(){
-        Slot slot = getInstanceOfSlot();
-        assertEquals(Slot.SlotType.CM, slot.getType());
-    }
-
-    @Test
-    void testClassroomEquality(){
-        Classroom classroom = getInstanceOfClassroom();
-        Slot slot = getInstanceOfSlot();
-        assertEquals(classroom, slot.getClassroom());
-    }
-
-    @Test
-    void testSubjectEquality(){
-        Subject subject = getInstanceOfSubject();
-        Slot slot = getInstanceOfSlot();
-        assertEquals(subject, slot.getSubject());
-    }
-
-    @Test
-    void testGroupEquality(){
-        Group group = getInstanceOfGroup();
-        Slot slot = getInstanceOfSlot();
-        assertEquals(group, slot.getGroup());
-    }
-
-    @Test
-    void testTimeRangeEquality(){
-        Slot slot = getInstanceOfSlot();
-        assertEquals(10, slot.getTimeRange());
     }
 }
