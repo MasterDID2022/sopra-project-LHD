0bea458061096fe38d890267972b0594d32f40ab
Desmond <desmondmies@gmail.com>
2022-12-09 14:39:03 +0100

    Entity long id implementation

--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/slots/Group.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/slots/Group.java
@@ -14,16 +14,21 @@ import java.util.List;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Group {
+
+    private long id;
+
     private final String name;
     private List<Student> students; //modify List<Student class>
 
     /**
-     * Factory for a Group with no students
+     * Factory for a Group given a list of students
+     * @param id long identifier for a Group (default -1)
      * @param name Group name
+     * @param students Group students list
      * @return an instance of a Group
      */
-    public static Group getInstance(String name){
-        return new Group(name, new ArrayList<>());
+    public static Group getInstance(long id, String name, List<Student> students) {
+        return new Group(id, name, students);
     }
 
     /**
@@ -33,7 +38,16 @@ public class Group {
      * @return an instance of a Group
      */
     public static Group getInstance(String name, List<Student> students) {
-        return new Group(name, students);
+        return new Group(-1, name, students);
+    }
+
+    /**
+     * Factory for a Group with no students
+     * @param name Group name
+     * @return an instance of a Group
+     */
+    public static Group getInstance(String name){
+        return getInstance(-1, name, new ArrayList<>());
     }
 
     /**
@@ -44,4 +58,6 @@ public class Group {
         student.add(this);
         this.students.add(student);
     }
+
+    public void setId(long id){ this.id = id; }
 }
--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/slots/Slot.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/slots/Slot.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.OptionalLong;
+
 /**
  * Class defining a Slot
  */
@@ -15,17 +18,20 @@ public class Slot {
 
     public enum SlotType { CM, TD, TP, EXAM, CONFERENCE, REUNION, OTHER }
 
-    private final SlotType type;
-    private final Classroom classroom;
-    private final Subject subject;
-    private final Group group;
+    private long id;
 
-    private int timeRange; //time range type needs to be changed to Time Range Wrapper Class
+    private final SlotType type;
+    private final long classroomId;
+    private final long subjectId;
+    private final List<Group> group;
+
+    private String timeRange; //time range type needs to be changed to Time Range Wrapper Class
 
     private String memo; //Slot annotation
 
     /**
      * Factory for a Slot class
+     * @param id long identifier for a Slot (default -1)
      * @param type SlotType enum, defines the type of this Slot
      * @param classroom Classroom object, to which Classroom this Slot is associated with
      * @param subject Subject object, what subject this Slot is about
@@ -34,8 +40,8 @@ public class Slot {
      * @param memo a little annotations for that Slot
      * @return an instance of Slot
      */
-    public static Slot getInstance(SlotType type, Classroom classroom, Subject subject, Group group, int timeRange, String memo){
-        return new Slot(type, classroom, subject, group, timeRange, memo);
+    public static Slot getInstance(long id, SlotType type, long classroom, long subject, List<Group> group, String timeRange, String memo){
+        return new Slot(id, type, classroom, subject, group, timeRange, memo);
     }
 
     /**
@@ -47,7 +53,20 @@ public class Slot {
      * @param timeRange indicates the Time taken by the Slot
      * @return an instance of Slot
      */
-    public static Slot getInstance(SlotType type, Classroom classroom, Subject subject, Group group, int timeRange){
-        return new Slot(type, classroom, subject, group, timeRange, null);
+    public static Slot getInstance(SlotType type, long classroom, long subject, List<Group> group, String timeRange){
+        return getInstance(-1, type, classroom, subject, group, timeRange, "");
+    }
+
+    /**
+     * Factory for a Slot class
+     * @param type SlotType enum, defines the type of this Slot
+     * @param classroom Classroom object, to which Classroom this Slot is associated with
+     * @param subject Subject object, what subject this Slot is about
+     * @param group Group object, to what group this Slot is for
+     * @param timeRange indicates the Time taken by the Slot
+     * @return an instance of Slot
+     */
+    public static Slot getInstance(SlotType type, long classroom, long subject, List<Group> group, String timeRange, String memo){
+        return getInstance(-1, type, classroom, subject, group, timeRange, memo);
     }
 }
