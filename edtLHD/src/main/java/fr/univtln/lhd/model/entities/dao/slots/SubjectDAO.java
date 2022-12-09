45a7e71f2d1429869ca2a01bf2b91a50bc81abf8
Desmond <desmondmies@gmail.com>
2022-12-09 10:57:56 +0100

    starting subject dao

--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/slots/SubjectDAO.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/slots/SubjectDAO.java
@@ -3,6 +3,10 @@ package fr.univtln.lhd.model.entities.dao.slots;
 import fr.univtln.lhd.model.entities.dao.DAO;
 import fr.univtln.lhd.model.entities.slots.Subject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +16,26 @@ import java.util.Optional;
  * SubjectDAO implementing DAO interface for Subject Object
  */
 public class SubjectDAO implements DAO<Subject> {
+
+    public static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    public static String USER = "";
+    public static String PASS = "";
+
+    private final Connection conn;
+    private final PreparedStatement getAll;
+    private final PreparedStatement get;
+    private final PreparedStatement save;
+    //private final PreparedStatement update;
+    //private final PreparedStatement delete;
+
+    private SubjectDAO() throws SQLException {
+        this.conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        this.get = conn.prepareStatement("SELECT * FROM SUBJECT WHERE ID=?");
+        this.getAll = conn.prepareStatement("SELECT * FROM SUBJECT");
+        this.save = conn.prepareStatement("INSERT INTO SUBJECT VALUES (?,?)");
+        //this.update = conn.prepareStatement("UPDATE SUBJECT ")
+    }
 
     /**
      * Getter for one Subject
