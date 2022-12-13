09b59a0a6800de3cbcf0c7af12c75f44999f91c1
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-12-13 11:19:07 +0000

    functional admin/manager before upate of BD

--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/user/AdminDAO.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/user/AdminDAO.java
@@ -3,7 +3,6 @@ package fr.univtln.lhd.model.entities.dao.user;
 import fr.univtln.lhd.exception.IdException;
 import fr.univtln.lhd.model.entities.dao.DAO;
 import fr.univtln.lhd.model.entities.user.Admin;
-import fr.univtln.lhd.model.entities.user.Lecturer;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
@@ -17,21 +16,23 @@ import java.util.Optional;
 
 @Slf4j
 public class AdminDAO implements DAO<Admin> {
-    private final Connection connection;
     private final PreparedStatement getAll;
     private final PreparedStatement get;
     private final PreparedStatement save;
+    private final PreparedStatement getIdFromEmail;
     private final PreparedStatement update;
     private final PreparedStatement delete;
 
     public AdminDAO() throws SQLException {
-        this.connection = initConnection();
+        Connection connection = initConnection();
         this.get = connection.prepareStatement("SELECT * FROM MANAGERS WHERE ID=?");
         this.getAll = connection.prepareStatement("SELECT * FROM MANAGERS");
-        this.save = connection.prepareStatement("INSERT INTO MANAGERS VALUES (DEFAULT, ?, ?, ?, ?)");
-        this.update = connection.prepareStatement("UPDATE MANAGERS SET name=?, fname=? ,email=?,WHERE ID=?");
+        this.save = connection.prepareStatement("INSERT INTO MANAGERS VALUES (DEFAULT, ?, ?, ?, ?, ?)");
+        this.update = connection.prepareStatement("UPDATE MANAGERS SET name=?, fname=? ,email=?,dpt=? WHERE ID=?");
         this.delete = connection.prepareStatement("DELETE FROM MANAGERS WHERE ID=?");
+        this.getIdFromEmail = connection.prepareStatement("SELECT ID FROM MANAGERS WHERE email=?");
     }
+
 
     /**
      * Getter for one Admin
@@ -50,12 +51,11 @@ public class AdminDAO implements DAO<Admin> {
                                 rs.getString("NAME"),
                                 rs.getString("FNAME"),
                                 rs.getString("EMAIL"),
-                                rs.getString("DPT")));
+                                rs.getString("TITLE"))
+                );
                 result.get().setId(rs.getLong("ID"));
             }
-        }catch (SQLException e){
-            log.error(e.getMessage());
-        } catch (IdException e) {
+        }catch (SQLException | IdException e){
             log.error(e.getMessage());
         }
         return result;
@@ -75,13 +75,11 @@ public class AdminDAO implements DAO<Admin> {
                                 rs.getString("NAME"),
                                 rs.getString("FNAME"),
                                 rs.getString("EMAIL"),
-                                rs.getString("DPT"));
+                                rs.getString("TITLE"));
                 admin.setId(rs.getLong("ID"));
                 adminList.add(admin);
             }
-        } catch (SQLException e){
-            log.error(e.getMessage());
-        } catch (IdException e) {
+        } catch (SQLException | IdException e){
             log.error(e.getMessage());
         }
         return adminList;
@@ -90,6 +88,9 @@ public class AdminDAO implements DAO<Admin> {
 
     /**
      * Save Admin t to Database
+     * <!>SHOULD ONLY BE USED FOR TEST</!>
+     * this methode save a User without a password
+     * please use save(Admin s,String password)
      * @param admin Admin object to save
      */
     @Override
@@ -104,49 +105,89 @@ public class AdminDAO implements DAO<Admin> {
         } catch (SQLException e){
             log.error(e.getMessage());
         }
-        log.error("Not suppossed to be used");
+        log.error("Not supposed to be used");
     }
 
     /**
-     * Save Admin t to Database
+     * Save Admin t to Database and add the ID generate by the database
+     * to admin, if the admin exist it will only update the ID
      * @param admin Admin object to save
      * @param password password to save inside the database
      */
     public void save(Admin admin, String password) {
+        ResultSet result;
         try{
             save.setString(1, admin.getName());
             save.setString(2, admin.getFname());
             save.setString(3, admin.getEmail());
             save.setString(4, password);
-            save.setString(5, admin.getFaculty());
+            save.setString(5, admin.getEmail());
             save.executeUpdate();
+
         } catch (SQLException e){
+            log.error(e.getMessage());
+        }
+        try{
+            getIdFromEmail.setString(1,admin.getEmail());
+            result = getIdFromEmail.executeQuery();
+            result.next();
+            admin.setId(result.getLong("ID"));
+        } catch (SQLException | IdException e) {
             log.error(e.getMessage());
         }
     }
 
     /**
-     * Update Data of Admin t
+     * Update the data of Admin in the database, without modifying the object admin,
+     * to get the new admin use <code>updateAndGet</code>
      * @param admin a Admin
      * @param params Map of attributes and values
      */
     @Override
-    public void update(Admin admin, Map params) {
+    public void update(Admin admin, Map params) throws IdException {
+        updateAndGet(admin,params);
+    }
+
+    /**
+     * Update Data of Admin t and return the new admin
+     * @param admin a Admin
+     * @param params Map of attributes and values
+     */
+    public Admin updateAndGet(Admin admin, Map<Object,Object> params) throws IdException {
+        String name = admin.getName();
+        String fname = admin.getFname();
+        String email = admin.getEmail();
+        String title = admin.getFaculty();
+        for (Object key : params.keySet()) {
+            if (key.equals("name")) {
+                name = params.get("name").toString();
+            } else if (key.equals("fname")) {
+                fname = params.get("fname").toString();
+            } else if (key.equals("email")) {
+                email = params.get("email").toString();
+            } else if (key.equals("title")) {
+                title = params.get("title").toString();
+            }
+        }
+        Admin updatedAdmin = Admin.of(name, fname,email,title);
+        updatedAdmin.setId(admin.getId());
         try {
-            update.setString(1, admin.getName());
-            update.setString(2, admin.getFname());
-            update.setString(3, admin.getEmail());
-            update.setString(5, admin.getFaculty());
-            update.setLong(3, admin.getId());
+            update.setString(1, updatedAdmin.getName());
+            update.setString(2, updatedAdmin.getFname());
+            update.setString(3,updatedAdmin.getEmail());
+            update.setLong(4, updatedAdmin.getId());
             update.executeUpdate();
+            return updatedAdmin;
         }
         catch (SQLException e){
             log.error(e.getMessage());
         }
+        log.error("did not update the database");
+        return admin;
     }
 
     /**
-     * Delete Admin t from Database
+     * Delete Admin from Database
      * @param admin Admin to be deleted from the database
      */
     @Override
