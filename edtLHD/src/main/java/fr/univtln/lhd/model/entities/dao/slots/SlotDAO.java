68766e0d030dbe0b790d685c64bf3a5688720a9c
Tom D <tom-domenge@etud.univ-tln.fr>
2022-12-19 18:57:07 +0100

    First DAO impl

--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/slots/SlotDAO.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/slots/SlotDAO.java
@@ -3,31 +3,35 @@ package fr.univtln.lhd.model.entities.dao.slots;
 import fr.univtln.lhd.exceptions.IdException;
 import fr.univtln.lhd.model.entities.dao.DAO;
 import fr.univtln.lhd.model.entities.dao.Datasource;
+import fr.univtln.lhd.model.entities.slots.Classroom;
+import fr.univtln.lhd.model.entities.slots.Group;
 import fr.univtln.lhd.model.entities.slots.Slot;
+import fr.univtln.lhd.model.entities.slots.Subject;
 import lombok.extern.slf4j.Slf4j;
+import org.threeten.extra.Interval;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-/**
- * SlotDAO implementing DAO interface for Slot Object
+/*
  */
 public class SlotDAO implements DAO<Slot> {
-
-    private final Connection conn;
-    //private final PreparedStatement getAll;
-    //private final PreparedStatement get;
-    //private final PreparedStatement save;
-    //private final PreparedStatement update;
-    //private final PreparedStatement delete;
-
-    private SlotDAO() throws SQLException {
-        conn = Datasource.getInstance().getConnection();
+    private SlotDAO () {
     }
+    static private final String  get = "select id,date_part('epoch',lower(timerange)) as begin_epoch, date_part('epoch',upper(timerange)) as end_epoch,classroom,memo,subject,type from slots WHERE id = (?)";
+    //private final String save = "INSERT INTO slots (DEFAULT,(?),(?),(?),(?),(?)";
+    //private final String  update;
+
+    //private final String delete = "DELETE FROM slots WHERE id=(?)";
+
+    private static SlotDAO getInstance () {
+        return new SlotDAO();
+    }
+
 
     /**
      * Getter for one Slot
@@ -35,9 +39,30 @@ public class SlotDAO implements DAO<Slot> {
      * @return May return one Slot instance
      */
     @Override
-    public Optional<Slot> get(long id) {
-        //wip
-        return Optional.empty();
+    public  Optional<Slot> get ( long id ){
+        try (Connection conn = Datasource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(get)
+        ){
+            stmt.setLong(1,id);
+            stmt.executeQuery();
+            ResultSet rs =stmt.getResultSet();
+            if(!rs.next()) {
+                return Optional.empty();
+            }
+            long fetchedID=rs.getLong(1);
+            final Instant fetchedBegin = Instant.ofEpochSecond(rs.getLong(2));
+            final Instant fetchedEnd = Instant.ofEpochSecond(rs.getLong(3));
+            final Interval fetchedInterval = Interval.of(fetchedBegin,fetchedEnd);
+            // final Classroom = ClassroomDAO.get(rs.getLong(4));
+            final Classroom fetchedClassroom = Classroom.getInstance("test");
+            final String fetchedMemo = rs.getString(5);
+            final Subject fetchedSubject = Subject.getInstance("e", 122);
+            // final Classroom = ClassroomDAO.get(rs.getLong(6));
+            final Slot.SlotType fetchedType = Slot.SlotType.valueOf(rs.getString(7));
+            return Optional.of(Slot.getInstance(fetchedID, fetchedType,fetchedClassroom, fetchedSubject, List.of(Group.getInstance("e")),fetchedInterval, fetchedMemo));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -62,7 +87,6 @@ public class SlotDAO implements DAO<Slot> {
     /**
      * Update Data of Slot Table
      * @param slot Slot instance to update
-     * @param params Map of attributes and values
      */
     @Override
     public Slot update(Slot slot) throws IdException {
