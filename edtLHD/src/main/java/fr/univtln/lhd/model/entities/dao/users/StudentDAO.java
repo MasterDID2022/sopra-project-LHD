2d4e78cb5343b7b859c2ac8be164402affdb3e65
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2022-12-21 16:50:48 +0100

    update StudentDao with try

--- edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/users/StudentDAO.java
+++ edtLHD/src/main/java/fr/univtln/lhd/model/entities/dao/users/StudentDAO.java
@@ -20,28 +20,27 @@ import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 @Slf4j
 public class StudentDAO implements DAO<Student> {
-    private final Connection connection;
-    private final PreparedStatement getAll;
-    private final PreparedStatement get;
-    private final PreparedStatement save;
-    private final PreparedStatement update;
-    private final PreparedStatement delete;
-    private final PreparedStatement getAllGroup;
-    private final PreparedStatement saveGroup;
+    private final String get="SELECT * FROM USERS WHERE ID=?";
+    private final String getAll="SELECT * FROM USERS";
+    private final String save="INSERT INTO GROUP_USER VALUES (?, ?)";
+    private final String update="UPDATE USERS SET name=?, fname=? ,email=? WHERE ID=?";
+    private final String delete="DELETE FROM USERS WHERE ID=?";
+    private final String getAllGroup="SELECT * FROM GROUP_USER WHERE ID_USER=?";
+    private final String saveGroup="INSERT INTO GROUP_USER VALUES (?, ?)";
 
-    private StudentDAO () throws SQLException {
-        this.connection = Datasource.getInstance().getConnection();
-        this.get = this.connection.prepareStatement("SELECT * FROM USERS WHERE ID=?");
-        this.getAll = this.connection.prepareStatement("SELECT * FROM USERS");
-        this.save = this.connection.prepareStatement("INSERT INTO USERS VALUES (DEFAULT, ?, ?, ?, ?)",RETURN_GENERATED_KEYS);
-        this.saveGroup = this.connection.prepareStatement("INSERT INTO GROUP_USER VALUES (?, ?)");
-        this.getAllGroup = this.connection.prepareStatement("SELECT * FROM GROUP_USER WHERE ID_USER=?");
-        this.update = this.connection.prepareStatement("UPDATE USERS SET name=?, fname=? ,email=? WHERE ID=?");
-        this.delete = this.connection.prepareStatement("DELETE FROM USERS WHERE ID=?");
-    }
+    private StudentDAO (){ }
 
-    public static StudentDAO getInstance () throws SQLException {
-        return new StudentDAO();
+    public static StudentDAO getInstance (){ return new StudentDAO(); }
+
+
+    /**
+     * Compile a string into a prepared statement
+     * @param statement
+     * @return the string into a PreparedStatement
+     */
+    private PreparedStatement CompileStatement( String statement) throws SQLException {
+        Connection connection = Datasource.getInstance().getConnection();
+        return connection.prepareStatement(statement);
     }
 
     /**
@@ -50,11 +49,18 @@ public class StudentDAO implements DAO<Student> {
      * @return May return one Student
      */
     @Override
-    public Optional<Student> get(long id) {
+    public Optional<Student> get(long id) throws SQLException {
+        PreparedStatement getDao; PreparedStatement getAllGroupDao;
+        try {
+            getDao = CompileStatement(get);
+            getAllGroupDao = CompileStatement(getAllGroup);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         Optional<Student> result = Optional.empty();
         try {
-            get.setLong(1, id);
-            ResultSet rs = get.executeQuery();
+            getDao.setLong(1, id);
+            ResultSet rs = getDao.executeQuery();
             if (rs.next()) {
                 result = Optional.of(
                         Student.of(
@@ -63,14 +69,7 @@ public class StudentDAO implements DAO<Student> {
                                 rs.getString("EMAIL"))
                 );
                 result.get().setId(rs.getLong("ID"));
-                long  IDgroupStudent;
-                getAllGroup.setLong(1,result.get().getId());
-                ResultSet rsGroup = getAllGroup.executeQuery();
-                GroupDAO dao = GroupDAO.getInstance();
-                while (rsGroup.next()) {
-                    IDgroupStudent = rsGroup.getLong("id_group");
-                    result.get().add(dao.get(IDgroupStudent).get());
-                }
+                updateStudentGroup(result.get());
             }
         }catch (SQLException | IdException e){
             log.error(e.getMessage());
@@ -79,28 +78,43 @@ public class StudentDAO implements DAO<Student> {
     }
 
     /**
+     * take a student and add every group that is in, by using the database
+     * @param student
+     */
+    private void updateStudentGroup(Student student) throws SQLException {
+        PreparedStatement getAllGroupDao = CompileStatement(getAllGroup);
+        getAllGroupDao.setLong(1,student.getId());
+        ResultSet resultSetGroupOfStudent = getAllGroupDao.executeQuery();
+        GroupDAO dao = GroupDAO.getInstance();
+        long  IDgroupStudent;
+        while (resultSetGroupOfStudent.next()) {
+            IDgroupStudent = resultSetGroupOfStudent.getLong("id_group");
+            student.add(dao.get(IDgroupStudent).get());
+        }
+    }
+
+    /**
      * Getter for all Student
      * @return List of all Student
      */
     @Override
-    public List<Student> getAll() {
+    public List<Student> getAll() throws SQLException {
         List<Student> studentList = new ArrayList<>();
-        long  IDgroupStudent;
+        PreparedStatement getAllDao;
         try {
-            ResultSet rs = getAll.executeQuery();
+            getAllDao = CompileStatement(getAll);
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        try {
+            ResultSet rs = getAllDao.executeQuery();
             while (rs.next()) {
                 Student student = Student.of(
                                 rs.getString("NAME"),
                                 rs.getString("FNAME"),
                                 rs.getString("EMAIL"));
                 student.setId(rs.getLong("ID"));
-                getAllGroup.setLong(1,student.getId());
-                ResultSet rsGroup = getAllGroup.executeQuery();
-                GroupDAO dao = GroupDAO.getInstance();
-                while (rsGroup.next()) {
-                    IDgroupStudent = rsGroup.getLong("id_group");
-                    student.add(dao.get(IDgroupStudent).get());
-                }
+                updateStudentGroup(student);
                 studentList.add(student);
             }
         } catch (SQLException | IdException e){
@@ -118,13 +132,15 @@ public class StudentDAO implements DAO<Student> {
      * @param student Student object to save
      */
     @Override
-    public void save(Student student) {
+    public void save(Student student) throws SQLException {
+        PreparedStatement saveDao;
+        saveDao = CompileStatement(save);
         try{
-            save.setString(1, student.getName());
-            save.setString(2, student.getFname());
-            save.setString(3, student.getEmail());
-            save.setString(4, "NO_PASSWORD");
-            save.executeUpdate();
+            saveDao.setString(1, student.getName());
+            saveDao.setString(2, student.getFname());
+            saveDao.setString(3, student.getEmail());
+            saveDao.setString(4, "NO_PASSWORD");
+            saveDao.executeUpdate();
         } catch (SQLException e){
             log.error(e.getMessage());
         }
@@ -137,28 +153,41 @@ public class StudentDAO implements DAO<Student> {
      * @param student Student object to save
      * @param password password to save inside the database
      */
-    public void save(Student student, String password) {
+    public void save(Student student, String password) throws SQLException {
         ResultSet result;
+        PreparedStatement saveDao;
+        saveDao = CompileStatement(save);
         try{
-            save.setString(1, student.getName());
-            save.setString(2, student.getFname());
-            save.setString(3, student.getEmail());
-            save.setString(4, password);
-            save.executeUpdate();
-            ResultSet id_set = save.getGeneratedKeys();
+            saveDao.setString(1, student.getName());
+            saveDao.setString(2, student.getFname());
+            saveDao.setString(3, student.getEmail());
+            saveDao.setString(4, password);
+            saveDao.executeUpdate();
+            ResultSet id_set = saveDao.getGeneratedKeys();
             id_set.next();
             student.setId(id_set.getLong(1));
-            if (student.getStudendGroup()!=null){
-                GroupDAO dao = GroupDAO.getInstance();
-                dao.save((Group) student.getStudendGroup());
-                saveGroup.setLong(1,((Group) student.getStudendGroup()).getId());
-                saveGroup.setLong(2,student.getId());
-                save.executeUpdate();
-            }
+
         } catch (SQLException | IdException e){
             log.error(e.getMessage());
         }
     }
+
+    /**
+     * save the group of the student with the Id inside group User
+     * @param student
+     * @throws SQLException
+     */
+    private void SaveStudentGroup(Student student) throws SQLException {
+        if (student.getStudendGroup()!=null){
+            GroupDAO dao = GroupDAO.getInstance();
+            dao.save((Group) student.getStudendGroup());
+            PreparedStatement saveGroupDao = CompileStatement(saveGroup);
+            saveGroupDao.setLong(1,((Group) student.getStudendGroup()).getId());
+            saveGroupDao.setLong(2,student.getId());
+            saveGroupDao.executeUpdate();
+        }
+    }
+
 
     /**
      * Update the data of Student in the database, without modifying the object student,
@@ -166,13 +195,15 @@ public class StudentDAO implements DAO<Student> {
      * @param student a Student
      */
     @Override
-    public Student update(Student student) throws IdException {
+    public Student update(Student student) throws SQLException {
+        PreparedStatement updateDao;
+        updateDao = CompileStatement(update);
         try {
-            update.setString(1,student.getName());
-            update.setString(2, student.getFname());
-            update.setString(3,student.getEmail());
-            update.setLong(4,student.getId());
-            update.executeUpdate();
+            updateDao.setString(1,student.getName());
+            updateDao.setString(2, student.getFname());
+            updateDao.setString(3,student.getEmail());
+            updateDao.setLong(4,student.getId());
+            updateDao.executeUpdate();
         }
         catch (SQLException e){
             log.error(e.getMessage());
@@ -185,10 +216,12 @@ public class StudentDAO implements DAO<Student> {
      * @param student Student to be deleted from the database
      */
     @Override
-    public void delete(Student student) {
+    public void delete(Student student) throws SQLException {
+        PreparedStatement deleteDao;
+        deleteDao = CompileStatement(delete);
         try {
-            delete.setLong(1,student.getId());
-            delete.executeUpdate();
+            deleteDao.setLong(1,student.getId());
+            deleteDao.executeUpdate();
         }
         catch (SQLException e){
             log.error(e.getMessage());
