5cf7dfa8a13f66edab99cc91d1ae2cf5aad1ccd4
Theo-Hafsaoui <theo.hafsaoui@laposte.net>
2023-01-05 22:11:17 +0100

    add authentification
    solve #47

--- edtLHD/src/main/java/fr/univtln/lhd/view/EdtApp.java
+++ edtLHD/src/main/java/fr/univtln/lhd/view/EdtApp.java
@@ -15,16 +15,15 @@ public class EdtApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         URL url = getClass().getResource("/fr.univtln.lhd.view/edt-lhd.fxml");
-
         FXMLLoader fxmlLoader = new FXMLLoader(url);
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 1100, 650);
-
         stage.setTitle("EDT LHD");
         scene.getStylesheets().add("/styles/edt-lhd-style.css");
         scene.getStylesheets().add("/styles/slot-info-panel-style.css");
         stage.setScene(scene);
-        stage.show();
+        //stage.setResizable(false); //need to be discussed
+        return;
     }
 
     public static void main(String[] args) {
